aa5c9c81b596e12cf2a1f9a07b36ee36eea9b8a4
Sebastian Baltes <research@sbaltes.com>
2025-10-21 22:00:20 +0200

    Initial commit for ISE assignment 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Software Engineering (ISE) Winter Semester 2025/26
+
+This repository is used for the version control assignment.
---
+++ src/main/java/de/seuhd/teaching/list/Main.java
@@ -0,0 +1,16 @@
+package de.seuhd.teaching.list;
+
+/**
+ * Main class for demo purposes.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        // Create a list and add some values...
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.append(new double[]{0.5, 4.2, 3.3, 0.9});
+
+        System.out.println("\nThe list contains the following elements:");
+        list.print();
+    }
+}
